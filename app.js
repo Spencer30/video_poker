8ee0cba5cdf508d8b098f0da505c007a8ed99c81
Spencer Lyons <sjl8r8@gmail.com>
2020-04-27 12:55:24 -0500

    created backend. going to push to heroku

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+let port = process.env.PORT;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+
+if (port == null || port == "") {
+    port = 9000;
+  }
+  
+  app.listen(port, () => {
+    console.log(`Server has started successfully on ${port}`);
+  });
